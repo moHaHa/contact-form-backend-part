49e27fccfe96e7eb186254890a792b1e5d56e65d
unknown <diab.4869@gmail.com>
2022-06-30 02:18:58 +0300

    Contact.cs Contact Model

---
+++ Contact.cs
@@ -0,0 +1,10 @@
+namespace ContactAPI
+{
+    public class Contact
+    {
+         public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+    }
+}
